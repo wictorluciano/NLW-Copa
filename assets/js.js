44504d923017453a814b2d92706b5313d058b4f1
Wictor Luciano Ferreira de Souza <wluciano01@gmail.com>
2022-11-18 14:50:04 -0300

    up

--- assets/js.js
+++ assets/js.js
@@ -7,10 +7,11 @@ function createGame(player1, hour, player2) {
         </li>
     `
 }
-
+let delay = -0.3;
 function createCard(date, day, games) {
+    delay = delay + 0.3;
     return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
         <h2>${date} <span>${day}</span></h2>
         <ul>
             ${games}
